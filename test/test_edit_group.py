3fd1e1dd443f6b86c92f5779dc9404964bb875ff
Nadezhda <Nadezhda.Volkova@artezio.com>
2019-12-09 22:31:37 +0300

    Intelectual Fixture for group

--- test/test_edit_group.py
+++ test/test_edit_group.py
@@ -1,8 +1,6 @@
 from model.group import Group
 
 def test_edit_group(app):
-    app.session.login(username="admin", password="secret")
     app.group.edit_group_from()
     app.group.fill_group_form(Group(name="Edited", header="edit description", footer="one number"))
     app.group.submit_changes_group()
-    app.session.logout()
