e9d3c875cfde492c2d4e930bb4b12c41810a9e9a
Nadezhda <Nadezhda.Volkova@artezio.com>
2019-12-09 17:56:15 +0300

    if-then-else for group modification

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -4,13 +4,13 @@ from model.group import Group
 def test_add_group(app):
     app.session.login(username="admin", password="secret")
     app.group.create()
-    app.group.filling_group_information(Group(name="1 G", header="description", footer="one"))
+    app.group.fill_group_form(Group(name="1 G", header="description", footer="one"))
     app.group.submit_group_creation()
     app.session.logout()
 
 def test_empty_group(app):
     app.session.login(username="admin", password="secret")
     app.group.create()
-    app.group.filling_group_information(Group(name="", header="", footer=""))
+    app.group.fill_group_form(Group(name="", header="", footer=""))
     app.group.submit_group_creation()
     app.session.logout()
