897857f97948fcdd3102070865a931d0518632f4
Nadezhda_Sokolova <Nadezhda.Volkova@artezio.com>
2019-12-19 13:36:00 +0300

    one more improvement for edit_group_test

--- test/test_edit_group.py
+++ test/test_edit_group.py
@@ -6,8 +6,13 @@ def test_edit_group(app):
         app.group.fill_group_form(Group(name='test'))
         app.group.submit_group_creation()
     old_groups = app.group.get_group_list()
+    group = Group(name="Edited", header="edit description", footer="one number")
+    group.id = old_groups[0].id
     app.group.edit_group_from()
-    app.group.fill_group_form(Group(name="Edited", header="edit description", footer="one number"))
+    app.group.fill_group_form(group)
     app.group.submit_changes_group()
     new_groups = app.group.get_group_list()
     assert len(old_groups) == len(new_groups)
+    old_groups[0] = group
+    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
+
