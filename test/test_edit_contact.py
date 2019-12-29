7940dd06c2b1df8cb5d89abd17d553a5f242d702
Nadezhda <Nadezhda.Volkova@artezio.com>
2019-12-29 16:03:33 +0300

    Improving for fax

--- test/test_edit_contact.py
+++ test/test_edit_contact.py
@@ -5,12 +5,12 @@ def test_edit_contact(app):
     if app.contacts.Count() == 0:
         app.contacts.New_contact_form()
         app.contacts.Filling_information_form(Contact(first_name="Delete name", last_name="For deleting name",
-                                                      home_phone="21323", work_phone="2393", mobile_phone = "24234324"))
+                                                      home_phone="21323", work_phone="2393", mobile_phone = "24234324", fax = '7777'))
         app.contacts.Submit_new_contact_creation()
         app.contacts.Open_home_page()
     old_contacts = app.contacts.get_contacts_list()
     index = randrange(len(old_contacts))
-    contact = Contact(first_name="Edited first name", last_name="Edited last name", home_phone="111", work_phone="222", mobile_phone="333")
+    contact = Contact(first_name="Edited first name", last_name="Edited last name", home_phone="111", work_phone="222", mobile_phone="333", fax = "0000")
     contact.id = old_contacts[index].id
     app.contacts.edit_contact_by_index(index)
     app.contacts.Filling_information_form(contact)
--- test/test_phones.py
+++ test/test_phones.py
@@ -6,7 +6,7 @@ def test_phones_on_home_page(app):
     assert contact_from_home_page.home_phone == clear(contact_from_edit_page.home_phone)
     assert contact_from_home_page.work_phone == clear(contact_from_edit_page.work_phone)
     assert contact_from_home_page.mobile_phone == clear(contact_from_edit_page.mobile_phone)
-    #assert contact_from_home_page.secondary_phone == contact_from_edit_page.secondary_phone
+
 
 def test_phones_on_contact_view_page(app):
     contact_from_view_page = app.contacts.get_contacts_from_view_page(0)
